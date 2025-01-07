943d2fd18bc286ced9f9cf651a0e3f9826eea293
Anton Serdyuchenko <anton415@gmail.com>
2025-01-07 14:48:17 +0300

    Adding ScrollView with Lazy VStack.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<40) {
+                    Text("Item \($0)")
+                }
+            }
+        }
         Image(.example)
             .resizable()
             .scaledToFit()
