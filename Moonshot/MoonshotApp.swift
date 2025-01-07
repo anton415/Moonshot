235b5a10675899d8691a04d4184286f16c3dc636
Anton Serdyuchenko <anton415@gmail.com>
2025-01-07 14:13:22 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Anton Serdyuchenko on 07.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
